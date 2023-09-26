43f2238349c25f2fab76ada38b03a16229087cde
Marat Musin <maratmusin@MacBook-Pro-Marat.local>
2023-09-26 19:03:36 +0300

    changed auth scenario

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,93 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { Suspense } from "react";
 import { oauthConfig } from "./shared/oauthConfig/oauthConfig";
 
-// Configuration for your OAuth 2.0 provider (Identity Server)
-
-function App() {
-  const [accessToken, setAccessToken] = useState<string | null>(null);
+function OAuthCallback() {
+  const location = useLocation();
 
   useEffect(() => {
-    // Check if an access token exists in cookies (user might be already authenticated)
-    const existingToken = Cookies.get("access_token");
-    if (existingToken) {
-      setAccessToken(existingToken);
-    }
-  }, []);
-
-  // Function to initiate the OAuth 2.0 authentication process
-  const initiateOAuth = () => {
-    // Generate a random state for CSRF protection (optional but recommended)
-    const state = Math.random().toString(36).substring(7);
-    Cookies.set("oauth_state", state, { secure: true, sameSite: "none" });
-
-    // Redirect the user to the authorization URL
-    const authorizationUrl = `${oauthConfig.authorizationUrl}?client_id=${
-      oauthConfig.clientId
-    }&redirect_uri=${encodeURIComponent(
-      oauthConfig.redirectUri
-    )}&response_type=code&scope=${oauthConfig.scopes.join(" ")}&state=${state}`;
-    window.location.href = authorizationUrl;
-  };
-
-  // Function to handle the OAuth callback and exchange the code for an access token
-  const handleOAuthCallback = async () => {
-    // Extract the authorization code from the URL query parameters
-    const params = new URLSearchParams(window.location.search);
+    const { search } = location;
+    const params = new URLSearchParams(search);
     const code = params.get("code");
     const state = params.get("state");
 
-    // Verify the state for CSRF protection
     const savedState = Cookies.get("oauth_state");
     if (state !== savedState) {
       console.error("OAuth state mismatch. Possible CSRF attack.");
       return;
     }
 
-    // Exchange the authorization code for an access token
-    try {
-      const response = await axios.post(oauthConfig.tokenUrl, {
-        code,
-        client_id: oauthConfig.clientId,
-        client_secret: oauthConfig.clientSecret,
-        redirect_uri: oauthConfig.redirectUri,
-        grant_type: "authorization_code",
-      });
+    (async () => {
+      try {
+        const { clientId, clientSecret, redirectUri } = oauthConfig;
+        const response = await axios.post(oauthConfig.tokenUrl, {
+          code,
+          client_id: clientId,
+          client_secret: clientSecret,
+          redirect_uri: redirectUri,
+          grant_type: "authorization_code",
+        });
 
-      // Store the access token securely in a cookie
-      const newAccessToken = response.data.access_token;
-      Cookies.set("access_token", newAccessToken, {
-        secure: true,
-        sameSite: "none",
-      });
+        const newAccessToken = response.data.access_token;
+        Cookies.set("access_token", newAccessToken, {
+          secure: true,
+          sameSite: "none",
+        });
+        Cookies.remove("oauth_state");
 
-      // Update the component state with the access token
-      setAccessToken(newAccessToken);
+        // Handle successful authentication (e.g., navigate to a protected route)
+        window.location.href = "/protected-route";
+      } catch (error) {
+        console.error("OAuth error:", error);
+      }
+    })();
+  }, [location]);
 
-      // Clear the state cookie
-      Cookies.remove("oauth_state");
+  return (
+    <div>
+      <p>Handling OAuth Callback...</p>
+    </div>
+  );
+}
 
-      // Redirect to a protected route or perform other actions
-      // Example: window.location.href = '/protected-route';
-    } catch (error) {
-      console.error("OAuth error:", error);
+function App() {
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const existingToken = Cookies.get("access_token");
+    if (existingToken) {
+      setAccessToken(existingToken);
     }
+  }, []);
+
+  const initiateOAuth = () => {
+    const state = Math.random().toString(36).substring(7);
+    Cookies.set("oauth_state", state, { secure: true, sameSite: "none" });
+
+    const { clientId, redirectUri, scopes } = oauthConfig;
+    const authorizationUrl = `${
+      oauthConfig.authorizationUrl
+    }?client_id=${clientId}&redirect_uri=${encodeURIComponent(
+      redirectUri
+    )}&response_type=code&scope=${scopes.join(" ")}&state=${state}`;
+    window.location.href = authorizationUrl;
   };
 
-  // Function to make authenticated GET requests to the API
   const fetchApiData = async () => {
     try {
       const response = await axios.get(
         "https://api.intelwash.ru/uitest/index.html",
         {
           headers: {
-            Authorization: `Bearer ${accessToken}`, // Include the access token in the Authorization header
+            Authorization: `Bearer ${accessToken}`,
           },
         }
       );
@@ -95,21 +100,63 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>OAuth 2.0 Authentication Example</h1>
-      {accessToken ? (
-        <div>
-          <p>Authenticated with Access Token</p>
-          <button onClick={fetchApiData}>Fetch API Data</button>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Router>
+        <div className="App">
+          <h1>OAuth 2.0 Authentication Example</h1>
+          <Routes>
+            <Route path="/auth-callback" element={<OAuthCallback />} />
+            <Route path="/protected-route">
+              {accessToken ? (
+                <>
+                  <div>Authenticated with Access Token</div>
+                  <button onClick={fetchApiData}>Fetch API Data</button>
+                </>
+              ) : (
+                <>Not Authenticated</>
+              )}
+            </Route>
+            <Route
+              path="/"
+              element={
+                <Home
+                  accessToken={accessToken}
+                  initiateOAuth={initiateOAuth}
+                  fetchApiData={fetchApiData}
+                />
+              }
+            />
+          </Routes>
         </div>
-      ) : (
-        <div>
-          <p>Not Authenticated</p>
-          <button onClick={initiateOAuth}>Login with OAuth</button>
-        </div>
-      )}
-    </div>
+      </Router>
+    </Suspense>
   );
+}
+
+function Home({
+  accessToken,
+  initiateOAuth,
+  fetchApiData,
+}: {
+  accessToken: string | null;
+  initiateOAuth: () => void;
+  fetchApiData: () => void;
+}) {
+  if (!accessToken) {
+    return (
+      <div>
+        <p>Not Authenticated</p>
+        <button onClick={initiateOAuth}>Login with OAuth</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>Authenticated with Access Token</p>
+        <button onClick={fetchApiData}>Fetch API Data</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/shared/oauthConfig/oauthConfig.ts
+++ src/shared/oauthConfig/oauthConfig.ts
@@ -3,6 +3,6 @@ export const oauthConfig = {
     clientSecret: 'UiTestClientSecret',
     authorizationUrl: 'https://sts-identity.intelwash.ru/connect/authorize',
     tokenUrl: 'https://sts-identity.intelwash.ru/connect/token',
-    redirectUri: 'https://identity-server-ten.vercel.app/auth/callback', // Replace with your app's domain
+    redirectUri: 'https://identity-server-ten.vercel.app/auth-callback', // Replace with your app's domain
     scopes: ['openid', 'email', 'profile', 'roles', 'permissions', 'UiTest.API'],
   };
