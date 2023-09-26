f8a84b32c38c60152a7cf6e6117423bde049bf77
Marat Musin <maratmusin@MacBook-Pro-Marat.local>
2023-09-26 19:29:58 +0300

    changed auth scenario

--- src/shared/oauthConfig/oauthConfig.ts
+++ src/shared/oauthConfig/oauthConfig.ts
@@ -1,8 +1,8 @@
 export const oauthConfig = {
-    clientId: 'UiTest.client',
-    clientSecret: 'UiTestClientSecret',
-    authorizationUrl: 'https://sts-identity.intelwash.ru/connect/authorize',
-    tokenUrl: 'https://sts-identity.intelwash.ru/connect/token',
-    redirectUri: 'https://identity-server-ten.vercel.app/auth-callback', // Replace with your app's domain
-    scopes: ['openid', 'email', 'profile', 'roles', 'permissions', 'UiTest.API'],
-  };
+  clientId: "UiTest.client",
+  clientSecret: "UiTestClientSecret",
+  authorizationUrl: "https://sts-identity.intelwash.ru/connect/authorize",
+  tokenUrl: "https://sts-identity.intelwash.ru/connect/token",
+  redirectUri: "https://identity-server-ten.vercel.app/oauth2-redirect.html", // Replace with your app's domain
+  scopes: ["openid", "email", "profile", "roles", "permissions", "UiTest.API"],
+};
